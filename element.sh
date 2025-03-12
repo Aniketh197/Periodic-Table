336fd8515022a2c14982f64b7e8504a84d5f4e89
Aniketh Nayak N <nayakaniketh970@gmail.com>
2025-03-12 08:59:53 +0000

    fix: Initial commit for query

--- element.sh
+++ element.sh
@@ -8,3 +8,18 @@ then
   echo "Please provide an element as an argument."
   exit 0
 fi
+# Determine if input is a number (atomic_number) or a string (name/symbol)
+if [[ $1 =~ ^[0-9]+$ ]]
+then
+  QUERY="SELECT atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius 
+         FROM elements 
+         JOIN properties USING(atomic_number) 
+         JOIN types USING(type_id) 
+         WHERE atomic_number = $1"
+else
+  QUERY="SELECT atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius 
+         FROM elements 
+         JOIN properties USING(atomic_number) 
+         JOIN types USING(type_id) 
+         WHERE symbol = '$1' OR name = '$1'"
+fi
