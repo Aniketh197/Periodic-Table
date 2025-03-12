d3cd8e78ec5d5e15ce6ea411cec96dc104738e75
Aniketh Nayak N <nayakaniketh970@gmail.com>
2025-03-12 09:00:32 +0000

    fix: Initial commit for query read and display

--- element.sh
+++ element.sh
@@ -23,3 +23,16 @@ else
          JOIN types USING(type_id) 
          WHERE symbol = '$1' OR name = '$1'"
 fi
+# Fetch element details
+ELEMENT=$($PSQL "$QUERY")
+
+# Check if element exists
+if [[ -z $ELEMENT ]]
+then
+  echo "I could not find that element in the database."
+  exit 0
+fi
+
+# Read and format output
+IFS="|" read -r ATOMIC_NUMBER NAME SYMBOL TYPE MASS MELTING BOILING <<< "$ELEMENT"
+echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $MASS amu. $NAME has a melting point of $MELTING celsius and a boiling point of $BOILING celsius."
